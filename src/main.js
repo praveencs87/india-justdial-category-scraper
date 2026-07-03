bc25f1c749e0cc8dd48e85743a516c5962124457
CoderBunch <praveencs87@gmail.com>
2026-07-03 09:34:38 +0530

    chore: enforce PPE, memory limits, and auto-abort

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { armKillSwitch, disarmKillSwitch } from './utils/timeoutManager.js';
 import { Actor, log } from 'apify';
 import { PlaywrightCrawler } from 'crawlee';
 
@@ -147,7 +148,9 @@ try {
         url: startUrl
     }]);
 
+    armKillSwitch(crawler);
     await crawler.run();
+    disarmKillSwitch();
 
     log.info(`🎉 Done! Extracted ${extractedCount} India Justdial leads.`);
 
